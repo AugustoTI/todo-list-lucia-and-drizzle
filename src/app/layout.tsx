db5f29ef7886889a918252acb919461522f01942
AugustoTI <augustocesar4001@gmail.com>
2024-03-02 15:16:19 -0300

    feat: add and configure shadcn-ui with dark mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,34 @@
-import '~/styles/global.css'
+import '@/styles/global.css'
 
 import { type ReactNode } from 'react'
 import { type Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { TailwindIndicator } from '@/components/tailwindcss-indicator'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
   description: 'This site was created using the NextJS framework 🚀',
 }
 
+const inter = Inter({ subsets: ['latin'], display: 'swap', style: 'normal' })
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className="antialiased">
-      <body>{children}</body>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={cn(inter.className, 'antialiased')}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <TailwindIndicator />
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { ThemeSwitch } from '@/components/theme-switch'
+
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+  return (
+    <>
+      <ThemeSwitch />
+    </>
+  )
 }
